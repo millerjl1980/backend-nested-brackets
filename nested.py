15f881f78b9f95ee986027943f310981bdd7f846
Justin Miller <indymiller@gmail.com>
2020-02-21 14:18:17 -0500

    is_nested function added

--- nested.py
+++ nested.py
@@ -3,21 +3,37 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Justin Miller"
+# Time to completed:  1 hour
+# https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-python/
+# referenced for building is_nested function arch
 
 import sys
 
 
-def is_nested(line):
-   print(line)
 
+def is_nested(line):
+    left_side =  ["[","{","("]
+    right_side = ["]","}",")"]
+    unmatched_list = []
+    error = 0
+    for char in line:
+        if char in left_side:
+            unmatched_list.append(char)
+        elif char in right_side:
+            loc = right_side.index(char)
+            if((len(unmatched_list)) > 0) and (left_side[loc] == unmatched_list[len(unmatched_list)-1]):
+                unmatched_list.pop()
+            else:
+                error = line.index(char)
+                print("NO " + str(error))
+    if len(unmatched_list) != 0:
+        print("No " + str((len(line))+1))
+    elif len(unmatched_list) == 0 and error == 0:
+        print("YES")
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
-#    if len(sys.argv) != 2:
-#         print 'usage: python nested.py file'
-#         sys.exit(1)
-
     filename = sys.argv[1]
     with open (filename, 'r') as rf:
         file_lines = rf.readlines()
