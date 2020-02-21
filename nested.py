ed591e6b48d623abe18228abcab53d1ae593e30e
Justin Miller <indymiller@gmail.com>
2020-02-21 13:29:42 -0500

    main function completed

--- nested.py
+++ nested.py
@@ -9,15 +9,20 @@ import sys
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+   print(line)
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
-    # Results: print to console and also write to output file
-    pass
+#    if len(sys.argv) != 2:
+#         print 'usage: python nested.py file'
+#         sys.exit(1)
 
+    filename = sys.argv[1]
+    with open (filename, 'r') as rf:
+        file_lines = rf.readlines()
+        for line in file_lines:
+            is_nested(line)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
